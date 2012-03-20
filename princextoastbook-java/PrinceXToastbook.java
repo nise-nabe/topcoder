c5a027fbd082d7e7b75f46352ed80fcb61190aec
nise_nabe <nise.nabe@gmail.com>
2012-03-20 16:30:40 +0900

    537 hard

--- princextoastbook-java/PrinceXToastbook.java
+++ princextoastbook-java/PrinceXToastbook.java
@@ -1,7 +1,24 @@
 public class PrinceXToastbook {
 
 	public double eat(int[] prerequisite) {
-		return 0.0;
+		int n = prerequisite.length;
+		double d = 0;
+		l: for (int i = 0; i < n; ++i) {
+			int p = prerequisite[i], c = 1;
+			while (p >= 0) {
+				p = prerequisite[p];
+				if (c > n) {
+					continue l;
+				}
+				++c;
+			}
+			long x = 1;
+			for (int j = 1; j <= c; ++j) {
+				x *= j;
+			}
+			d += 1 / (double) x;
+		}
+		return d;
 	}
 
 }
