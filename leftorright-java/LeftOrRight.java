cfc7de254ce179f605c50d257522dbcfedef18a3
nise_nabe <nise.nabe@gmail.com>
2012-03-21 10:42:00 +0900

    fixed 538 easy

--- leftorright-java/LeftOrRight.java
+++ leftorright-java/LeftOrRight.java
@@ -1,27 +1,16 @@
 public class LeftOrRight {
- 
-  public int maxDistance(String p) {
-    return maxDist(p, 0, 0);
-  }
- 
-  int max = 0;
- 
-  int maxDist(String p, int x, int h) {
-    if (p.length() != 0) {
-      String ss = p.substring(1);
-      if (p.charAt(0) == '?') {
-        if (h == 0) {
-          maxDist(ss, x + 1, 1);
-          maxDist(ss, x - 1, -1);
-        } else {
-          maxDist(ss, x + h, h);
-        }
-      } else if (p.charAt(0) == 'R') {
-        maxDist(ss, x + 1, 0);
-      } else {
-        maxDist(ss, x - 1, 0);
-      }
-    }
-    return max = Math.max(max, Math.abs(x));
-  }
+
+	public int maxDistance(String p) {
+		return Math.max(maxDist(p.replace('?', 'L'), 0, 0),
+				maxDist(p.replace('?', 'R'), 0, 0));
+	}
+
+	int max = 0;
+
+	int maxDist(String p, int x, int h) {
+		if (p.length() != 0) {
+			maxDist(p.substring(1), x + (p.charAt(0) == 'R' ? 1 : -1), 0);
+		}
+		return max = Math.max(max, Math.abs(x));
+	}
 }
